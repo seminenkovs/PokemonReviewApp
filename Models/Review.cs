c834065d2972f4cd8af0e4c49be3066ff76bc228
Vitalii <seminenkovs@gmail.com>
2023-10-14 22:37:52 +0200

    Created Review and Reviewer Models

---
+++ Models/Review.cs
@@ -0,0 +1,8 @@
+﻿namespace PokemonReviewApp.Models;
+
+public class Review
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Text { get; set; }
+}
---
+++ Models/Reviewer.cs
@@ -0,0 +1,8 @@
+﻿namespace PokemonReviewApp.Models;
+
+public class Reviewer
+{
+    public int Id { get; set; }
+    public string FistName { get; set; }
+    public string LastName { get; set; }
+}
