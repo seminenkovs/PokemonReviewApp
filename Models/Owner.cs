a7ce4c1adbe26d24edc018686e5b7540851282e1
Vitalii <seminenkovs@gmail.com>
2023-10-14 23:25:18 +0200

    Created many to many relations between Pokemon and Category, Pokemon and Owner

--- Models/Owner.cs
+++ Models/Owner.cs
@@ -6,4 +6,5 @@ public class Owner
     public string Name { get; set; }
     public string Gym { get; set; }
     public Country Country { get; set; }
+    public ICollection<PokemonOwner> PokemonOwners { get; set; }
 }
--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -6,4 +6,6 @@ public class Pokemon
     public string Name { get; set; }
     public DateTime BirthDate { get; set; }
     public ICollection<Review> Reviews { get; set; }
+    public ICollection<PokemonOwner> PokemonOwners { get; set; }
+    public ICollection<PokemonCategory> PokemonCategories { get; set; }
 }
