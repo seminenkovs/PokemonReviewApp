2959d97cd35e862ac95c4d83104bd2c532215a8d
Vitalii <seminenkovs@gmail.com>
2023-10-15 00:10:14 +0200

    Changes:  to owner model added First and Last name fields to review model added Rating field

--- Models/Reviewer.cs
+++ Models/Reviewer.cs
@@ -3,7 +3,7 @@
 public class Reviewer
 {
     public int Id { get; set; }
-    public string FistName { get; set; }
+    public string FirstName { get; set; }
     public string LastName { get; set; }
     public ICollection<Review> Reviews { get; set; }
 }
