e147ff6190cd8b0b6cf42405da0ad7f41e77ceb2
Vitalii <seminenkovs@gmail.com>
2023-11-05 20:03:53 +0100

    Created ReviewRepository

---
+++ Repository/ReviewRepository.cs
@@ -0,0 +1,35 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository;
+
+public class ReviewRepository : IReviewRepository
+{
+    private readonly DataContext _context;
+
+    public ReviewRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public ICollection<Review> GetReviews()
+    {
+        return _context.Reviews.ToList();
+    }
+
+    public Review GetReview(int reviewId)
+    {
+        return _context.Reviews.Where(r => r.Id == reviewId).FirstOrDefault();
+    }
+
+    public ICollection<Review> GetReviewsOfPokemon(int pokemonId)
+    {
+        return _context.Reviews.Where(r => r.Pokemon.Id == pokemonId).ToList();
+    }
+
+    public bool ReviewExists(int reviewId)
+    {
+        return _context.Reviews.Any(r => r.Id == reviewId);
+    }
+}
