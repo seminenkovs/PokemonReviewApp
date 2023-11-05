ce6bd55f7193673bd3c93c583f5a144bb82484d7
Vitalii <seminenkovs@gmail.com>
2023-11-05 02:53:23 +0100

    PokemonRepository implemented Interface

--- Repository/PokemonRepository.cs
+++ Repository/PokemonRepository.cs
@@ -13,8 +13,35 @@ public class PokemonRepository : IPokemonRepository
         _context = context;
     }
 
+    public Pokemon GetPokemon(int pokemonId)
+    {
+        return _context.Pokemon.Where(p => p.Id == pokemonId).FirstOrDefault();
+    }
+
+    public Pokemon GetPokemon(string name)
+    {
+        return _context.Pokemon.Where(p => p.Name == name).FirstOrDefault();
+    }
+
+    public decimal GetPokemonRating(int pokemonId)
+    {
+        var review = _context.Reviews.Where(p => p.Id == pokemonId);
+
+        if (review.Count() <= 0 )
+        {
+            return 0;
+        }
+
+        return ((decimal)review.Sum(r => r.Rating) / review.Count());
+    }
+
     public ICollection<Pokemon> GetPokemons()
     {
         return _context.Pokemon.OrderBy(p => p.Id).ToList();
+    }
+
+    public bool PokemonExists(int pokemonId)
+    {
+        return _context.Pokemon.Any(p => p.Id == pokemonId);
     }
 }
