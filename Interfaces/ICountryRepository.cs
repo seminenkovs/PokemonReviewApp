e31fbd84f4c45283e16a6ff9a7fdd3097641866a
Vitalii <seminenkovs@gmail.com>
2023-11-05 04:35:46 +0100

    Created ICountryRepository

---
+++ Interfaces/ICountryRepository.cs
@@ -0,0 +1,12 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces;
+
+public interface ICountryRepository
+{
+    ICollection<Country> GetCountries();
+    Country GetCountry(int countryId);
+    Country GetCountryByOwner(int ownerId);
+    ICollection<Owner> GetOwnersFromCountry(int countryId);
+    bool CountryExists(int countryId);
+}
