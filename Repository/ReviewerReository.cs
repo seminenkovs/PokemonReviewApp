9162ae25e2370ecc8745ac13e8b724c5a881d5f6
Vitalii <seminenkovs@gmail.com>
2023-11-05 20:39:54 +0100

    Created ReviewerReository

---
+++ Repository/ReviewerReository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository;
+
+public class ReviewerReository : IReviewerReository
+{
+    private readonly DataContext _context;
+
+    public ReviewerReository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public ICollection<Reviewer> GetReviewers()
+    {
+        return _context.Reviewser.ToList();
+    }
+
+    public Reviewer GetReviewer(int reviewerId)
+    {
+        return _context.Reviewser.Where(r => r.Id == reviewerId)
+            .Include(e => e.Reviews).FirstOrDefault();
+    }
+
+    public ICollection<Review> GetReviewsByReviewer(int reviewerId)
+    {
+        return _context.Reviews.Where(r => r.Reviewer.Id == reviewerId).ToList();
+    }
+
+    public bool ReviewerExists(int reviewerId)
+    {
+        return _context.Reviewser.Any(r => r.Id == reviewerId);
+    }
+}
