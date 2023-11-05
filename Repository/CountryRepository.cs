ef97b22722b88e261e871fc42afa09e128f18a0a
Vitalii <seminenkovs@gmail.com>
2023-11-05 04:47:46 +0100

    Created CountryRepository Implemented ICountryRepository

---
+++ Repository/CountryRepository.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public CountryRepository(DataContext _context, IMapper mapper)
+        {
+            this._context = _context;
+            _mapper = mapper;
+        }
+        public bool CountryExists(int countryId)
+        {
+            return _context.Countries.Any(c => c.Id == countryId);
+        }
+
+        public ICollection<Country> GetCountries()
+        {
+            return _context.Countries.ToList();
+        }
+
+        public Country GetCountry(int countryId)
+        {
+            return _context.Countries.Where(c => c.Id == countryId).FirstOrDefault();
+        }
+
+        public Country GetCountryByOwner(int ownerId)
+        {
+            return _context.Owners.Where(o => o.Id == ownerId).Select(c => c.Country).FirstOrDefault();
+        }
+
+        public ICollection<Owner> GetOwnersFromCountry(int countryId)
+        {
+            return _context.Owners.Where(o => o.Country.Id == countryId).ToList();
+        }
+    }
+}
