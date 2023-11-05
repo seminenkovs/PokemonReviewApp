4bf397d75c87804c635d4ae68ee2af7acc9b40e5
Vitalii <seminenkovs@gmail.com>
2023-11-05 17:53:15 +0100

    Created IOwnerRepository and OwnerRepository,  implemented IOwnerRepository by OwnerRepository

---
+++ Interfaces/IOwnerRepository.cs
@@ -0,0 +1,12 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces;
+
+public interface IOwnerRepository
+{
+    ICollection<Owner> GetOwners();
+    Owner GetOwner(int ownerId);
+    ICollection<Owner> GetOwnersOfPokemon(int pokemonId);
+    ICollection<Pokemon> GetPokemonByOwner(int ownerId);
+    bool OwnerExists(int ownerId);
+}
---
+++ Repository/OwnerRepository.cs
@@ -0,0 +1,42 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository;
+
+public class OwnerRepository : IOwnerRepository
+{
+    private readonly DataContext _context;
+
+    public OwnerRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public Owner GetOwner(int ownerId)
+    {
+        return _context.Owners.Where(o => o.Id == ownerId).FirstOrDefault();
+    }
+
+    public ICollection<Owner> GetOwners()
+    {
+        return _context.Owners.ToList();
+    }
+
+    public ICollection<Owner> GetOwnersOfPokemon(int pokemonId)
+    {
+        return _context.PokemonOwners.Where(p => p.Pokemon.Id == pokemonId)
+            .Select(o => o.Owner).ToList();
+    }
+
+    public ICollection<Pokemon> GetPokemonByOwner(int ownerId)
+    {
+        _context.PokemonOwners.Where(o => o.Owner.Id == ownerId)
+            .Select(p => p.Pokemon).ToList();
+    }
+
+    public bool OwnerExists(int ownerId)
+    {
+        return _context.Owners.Any(o => o.Id == ownerId);
+    }
+}
