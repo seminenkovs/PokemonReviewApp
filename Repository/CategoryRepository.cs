746d1f4975e6e577d0c4ced295b12b9bd05dbaf1
Vitalii <seminenkovs@gmail.com>
2023-11-07 03:49:32 +0100

    CategoryRepository Implemented Save() and CreateCategory

--- Repository/CategoryRepository.cs
+++ Repository/CategoryRepository.cs
@@ -32,4 +32,16 @@ public class CategoryRepository : ICategoryRepository
     {
         return _context.PokemonCategories.Where(c => c.CategoryId == categoryId).Select(c => c.Pokemon).ToList();
     }
+
+    public bool CreateCategory(Category category)
+    {
+        _context.Add(category);
+        return Save();
+    }
+
+    public bool Save()
+    {
+        var saved = _context.SaveChanges();
+        return saved > 0 ? true : false;
+    }
 }
