2b099006a1bf0eddde21742bfe17f64e7da950d6
Vitalii <seminenkovs@gmail.com>
2023-11-08 03:43:50 +0100

    OwnerRepository implemented CreateOwner and Save

--- Repository/OwnerRepository.cs
+++ Repository/OwnerRepository.cs
@@ -39,4 +39,16 @@ public class OwnerRepository : IOwnerRepository
     {
         return _context.Owners.Any(o => o.Id == ownerId);
     }
+
+    public bool CreateOwner(Owner owner)
+    {
+        _context.Add(owner);
+        return Save();
+    }
+
+    public bool Save()
+    {
+        var saved = _context.SaveChanges();
+        return saved > 0 ? true : false;
+    }
 }
