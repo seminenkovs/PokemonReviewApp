0f788343bec2ad771be1e66f9690c91d124c7730
Vitalii <seminenkovs@gmail.com>
2023-11-13 19:40:56 +0100

    PokemonRepository implemented Save and CreatePokemon

--- Repository/PokemonRepository.cs
+++ Repository/PokemonRepository.cs
@@ -44,4 +44,36 @@ public class PokemonRepository : IPokemonRepository
     {
         return _context.Pokemon.Any(p => p.Id == pokemonId);
     }
+
+    public bool CreatePokemon(int ownerId, int categoryId, Pokemon pokemon)
+    {
+        var pokemonOwnerEntity = _context.Owners.Where(o => o.Id == ownerId).FirstOrDefault();
+        var category = _context.Categories.Where(c => c.Id == categoryId).FirstOrDefault();
+
+        var pokemonOwner = new PokemonOwner()
+        {
+            Owner = pokemonOwnerEntity,
+            Pokemon = pokemon
+        };
+
+        _context.Add(pokemonOwner);
+
+        var pokemonCategory = new PokemonCategory()
+        {
+            Category = category,
+            Pokemon = pokemon
+        };
+
+        _context.Add(pokemonCategory);
+
+        _context.Add(pokemon);
+
+        return Save();
+    }
+
+    public bool Save()
+    {
+        var save = _context.SaveChanges();
+        return save > 0 ? true : false;
+    }
 }
