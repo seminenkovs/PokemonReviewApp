c1b93a3c1fd6bdaf1076b2f711e9d7414378a13c
Vitalii <seminenkovs@gmail.com>
2023-12-03 01:52:14 +0100

    CountryController added UpdateCountry

--- Controllers/CuntryController.cs
+++ Controllers/CuntryController.cs
@@ -103,5 +103,42 @@ namespace PokemonReviewApp.Controllers
 
             return Ok("Seccessfully created");
         }
+
+        [HttpPut("{countryId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult UpdateCountry(int countryId, [FromBody] CountryDto updatedCountry)
+        {
+            if (updatedCountry == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (countryId != updatedCountry.Id)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!_countryRepository.CountryExists(countryId))
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var countryMap = _mapper.Map<Country>(updatedCountry);
+
+            if (!_countryRepository.UpdateCountry(countryMap))
+            {
+                ModelState.AddModelError("", "Something went wrong updating country");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+        }
     }
 }
