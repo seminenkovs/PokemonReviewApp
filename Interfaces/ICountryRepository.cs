c174ebc223f3353a0d1e066537a4e9e3d7031675
Vitalii <seminenkovs@gmail.com>
2023-12-03 01:46:31 +0100

    ICountryRepository added UpdateCountry

--- Interfaces/ICountryRepository.cs
+++ Interfaces/ICountryRepository.cs
@@ -10,5 +10,6 @@ public interface ICountryRepository
     ICollection<Owner> GetOwnersFromCountry(int countryId);
     bool CountryExists(int countryId);
     bool CreateCountry(Country country);
+    bool UpdateCountry(Country country);
     bool Save();
 }
