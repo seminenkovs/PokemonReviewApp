fbc61217187694b5dfc3be811686325eab61ff4a
Vitalii <seminenkovs@gmail.com>
2023-12-03 19:11:34 +0100

    ReviewerRepository implemented UpdateReviewer

--- Repository/ReviewerRepository.cs
+++ Repository/ReviewerRepository.cs
@@ -41,6 +41,12 @@ public class ReviewerRepository : IReviewerRepository
         return Save();
     }
 
+    public bool UpdateReviewer(Reviewer reviewer)
+    {
+        _context.Update(reviewer);
+        return Save();
+    }
+
     public bool Save()
     {
         var saved = _context.SaveChanges();
