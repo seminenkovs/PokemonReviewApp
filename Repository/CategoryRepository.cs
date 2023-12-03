6c08f8c1df0a5c131a2efcf714fbe84c8758aa3a
Vitalii <seminenkovs@gmail.com>
2023-12-03 19:31:58 +0100

    CategoryRepository implemented DeleteCategory

--- Repository/CategoryRepository.cs
+++ Repository/CategoryRepository.cs
@@ -45,6 +45,13 @@ public class CategoryRepository : ICategoryRepository
         return Save();
     }
 
+    public bool DeleteCategory(Category category)
+    {
+        _context.Remove(category);
+        return Save();
+
+    }
+
     public bool Save()
     {
         var saved = _context.SaveChanges();
