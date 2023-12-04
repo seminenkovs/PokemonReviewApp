1f556a1e5288d2553eaf694ebf2b2dec1049f6c1
Vitalii <seminenkovs@gmail.com>
2023-12-04 18:32:23 +0100

    IOwnerRepository added DeleteOwner

--- Interfaces/IOwnerRepository.cs
+++ Interfaces/IOwnerRepository.cs
@@ -11,5 +11,6 @@ public interface IOwnerRepository
     bool OwnerExists(int ownerId);
     bool CreateOwner(Owner owner);
     bool UpdateOwner (Owner owner);
+    bool DeleteOwner (Owner owner);
     bool Save();
 }
