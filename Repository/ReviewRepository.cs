cfe6e4b5ab7b8fa42b33489547efda287637457d
Vitalii <seminenkovs@gmail.com>
2023-12-04 19:12:04 +0100

    ReviewRepository implemented DeleteReviews and DeleteteReview

--- Repository/ReviewRepository.cs
+++ Repository/ReviewRepository.cs
@@ -46,6 +46,18 @@ public class ReviewRepository : IReviewRepository
         return Save();
     }
 
+    public bool DeleteReview(Review review)
+    {
+        _context.Remove(review);
+        return Save();
+    }
+
+    public bool DeleteReviews(List<Review> reviews)
+    {
+        _context.RemoveRange(reviews);
+        return Save();
+    }
+
     public bool Save()
     {
         var saved = _context.SaveChanges();
