8c4b5f66118c5e09eee941d867a88181bc28d5d3
Vitalii <seminenkovs@gmail.com>
2023-12-04 19:09:22 +0100

    IReviewRepository added DeleteReview and DeleteReviews

--- Interfaces/IReviewRepository.cs
+++ Interfaces/IReviewRepository.cs
@@ -10,5 +10,7 @@ public interface IReviewRepository
     bool ReviewExists(int reviewId);
     bool CreateReview(Review review);
     bool UpdateReview(Review review);
+    bool DeleteReview(Review review);
+    bool DeleteReviews(List<Review> reviews);
     bool Save();
 }
