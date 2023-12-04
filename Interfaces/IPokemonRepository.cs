33d92f4ff100de6b4d99fc6cc88d57082a313e1c
Vitalii <seminenkovs@gmail.com>
2023-12-04 18:55:19 +0100

    IPokemonRepository added DeletePokemon

--- Interfaces/IPokemonRepository.cs
+++ Interfaces/IPokemonRepository.cs
@@ -11,5 +11,6 @@ public interface IPokemonRepository
     bool PokemonExists(int pokemonId);
     bool CreatePokemon(int ownerId, int categoryId, Pokemon pokemon);
     bool UpdatePokemon(int ownerId, int categoryId, Pokemon pokemon);
+    bool DeletePokemon(Pokemon pokemon);
     bool Save();
 }
